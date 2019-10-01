0114aa657cb5ddad7bd9f8de7b11a81d2c377da6
Nguyễn Thành Nhân <nhocksok1009@gmail.com>
2019-10-01 17:34:47 +0700

    Ceil , Floor

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+   public  class Program
+    {
+       //input :float x
+       //output : int a that is smallest but not smaller than a
+       //idea : a = (int) x then a=a+1\
+       //if : x negative then a is unchanged
+       //if x==a then a is unchanged
+       public static void Main(string[] args)
+        {
+        }
+        public static int Ceil(float x)
+        {    
+           /*  if(x<0)  
+            
+                return (int)x;
+
+            return ((int)x) + 1;   
+            */
+            int a = (int)x;
+            if(x>0)
+                if(x>a)
+                a = a + 1;
+            return a;
+
+        }
+       //input: float y
+        //output : int a that is largest but not smaller than a
+        //idea : a = (int) x then a=a-1
+        //if : x negative then a is unchanged
+        //if x==a then a is unchanged
+       public static int Floor(float y)
+        {
+            int a = (int)y;
+            if (y < 0 && y!=a)              
+                    a = a - 1;                
+            return a;
+        }
+
+       
+    }
+}
---
+++ ConsoleApplication3/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication3;
+using System.IO;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCeil()
+        {
+            int result1 = Program.Ceil((float)2.3);
+            Assert.AreEqual(3, result1);
+
+            int rs2 = Program.Ceil((float)-2.3f);
+            Assert.AreEqual(-2, rs2);
+
+            int rs3 = Program.Ceil(3);
+            Assert.AreEqual(3, rs3);
+
+        }
+        [TestMethod]
+        public void TestFloor()
+        {
+            int result2 = Program.Floor((float)2.3);
+            Assert.AreEqual(2, result2);
+
+            int rs3 = Program.Floor((float)-2.3f);
+            Assert.AreEqual(-3, rs3);
+
+            int rs4 = Program.Floor(3);
+            Assert.AreEqual(3, rs4);
+        }
+
+    }
+
+}
