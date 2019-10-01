9d3f7dba918fca702d7881944d2f889e0a6e512b
Nguyễn Thành Nhân <nhocksok1009@gmail.com>
2019-10-01 15:57:02 +0700

    Exercide4-1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+
+        }
+       public static bool IsOdd(int n)
+        {
+            if (n % 2 == 0)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+using System.IO;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool res3 = Program.IsOdd(3);
+            Assert.AreEqual(true, res3);
+        }
+    }
+}
